c8d9eb1d2895afc3823888ca06bc5d346d475a71
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-20 10:27:12 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,3 @@
+from cx_Freeze import setup, Executable
 
+setup(name="Batch image resizer", executables=[Executable("Batch image resizer script.py")], options={"build_exe": {"excludes": ["tkinter"]}})
